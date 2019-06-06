7173981b0c22e9e735c85cfc6d3ca621a161ecfa
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-06-06 14:02:49 +0530

    Add files via upload

---
+++ set 6b.py
@@ -0,0 +1,13 @@
+#anju
+x,y=map(int,raw_input().split())
+s,t=map(int,raw_input().split())
+u,v=map(int,raw_input().split())
+w,z=map(int,raw_input().split())
+i=t-y
+j=v-z
+k=u-s
+l=w-x
+if i==j== k==l:
+	print("yes")
+else:
+	print("no")
