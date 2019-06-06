f26205728f7beb66cdbc3e352ba91328c9d1def1
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-06-06 14:09:55 +0530

    Update set 6b.py

--- set 6b.py
+++ set 6b.py
@@ -1,12 +1,12 @@
 #anju
-x,y=map(int,raw_input().split())
-s,t=map(int,raw_input().split())
-u,v=map(int,raw_input().split())
-w,z=map(int,raw_input().split())
-i=t-y
-j=v-z
-k=u-s
-l=w-x
+A,B=map(int,input().split())
+C,D=map(int,input().split())
+E,F=map(int,input().split())
+G,H=map(int,input().split())
+i=D-B
+j=F-H
+k=E-C
+l=G-A
 if i==j== k==l:
 	print("yes")
 else:
