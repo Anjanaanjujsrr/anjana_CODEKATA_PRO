8bbac34d2f78ac4ffc1f7c06908a96761f58240f
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-06-06 14:15:36 +0530

    Update set 6c.py

--- set 6c.py
+++ set 6c.py
@@ -4,7 +4,7 @@ a=a.lower()
 l=len(a)
 x=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 for i in range(0,l):
-	if(x[i] in a):
+	if(a[i] in x):
 		x.remove(a[i])
 if(len(x)==0):
 	print("yes")
