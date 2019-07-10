22515d948fb48665888d5e43d12bcc46382d0554
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-07-10 19:42:21 +0530

    Update set 3h.py

--- set 3h.py
+++ set 3h.py
@@ -1,11 +1,11 @@
 #anju
-num=int(input())
-m1=list(map(int,input().split()))
-m1.sort()
-sin=0
-c1=0
-for i in range(len(m1)):
-    if m1[i]>=s:
-        c1=c1+1
-    s=s+m1[i]
-print(c1)
+p=int(input())
+n=list(map(int,input().split()))
+n.sort()
+s=0
+sv=0
+for i in range(len(n)):
+    if n[i]>=s:
+        sv=sv+1
+    s=s+n[i]
+print(sv)
