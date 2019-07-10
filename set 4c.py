f90b404e228aa31a315018d314e3ca741d4c6eda
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-07-10 19:58:56 +0530

    Update set 4c.py

--- set 4c.py
+++ set 4c.py
@@ -7,7 +7,7 @@ for i in range(a):
 for i in range(a):
       for j in range(b-1):
             if l[i][j] != 'R' and l[i][j+1] != 'R' :
-                  cost+=3
+                  cos+=3
             elif l[i][j] != 'G' and l[i][j+1] != 'G':
                   cos+=5
 print(cos)
